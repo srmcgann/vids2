500341fd1c7dfe9c8e99096636bc52f5d34c2deb
scott mcgann <s.r.mcgann@hotmail.com>
2023-07-05 04:59:03 +0000

    initial commit

---
+++ file.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+yt-dlp https://m.youtube.com/watch?v=tWnayRpHgLg --remux-video mp4
+yt-dlp https://m.youtube.com/watch?v=HuipBLsURtU --remux-video mp4
+yt-dlp https://m.youtube.com/watch?v=JdsmPvZyYCo --remux-video mp4
+yt-dlp https://m.youtube.com/watch?v=ti2kx7vmTWE --remux-video mp4
+yt-dlp https://m.youtube.com/watch?v=NiNGJ8iB81c --remux-video mp4
+yt-dlp https://m.youtube.com/watch?v=3hDyMiMNDcU --remux-video mp4
+yt-dlp https://m.youtube.com/watch?v=IvVOxEwWzL4 --remux-video mp4
+yt-dlp https://m.youtube.com/watch?v=qnO9ipZm9UY --remux-video mp4
+yt-dlp https://m.youtube.com/watch?v=lZjqj-nnl1w --remux-video mp4
+yt-dlp https://m.youtube.com/watch?v=idu7koS1BtI --remux-video mp4
+yt-dlp https://m.youtube.com/watch?v=2ki4f9ObP58 --remux-video mp4
+yt-dlp https://m.youtube.com/watch?v=MQjc35p78wE --remux-video mp4
+yt-dlp https://m.youtube.com/watch?v=etHBEuwHn8Y --remux-video mp4
+yt-dlp https://m.youtube.com/watch?v=1nagfpQruYM --remux-video mp4
+yt-dlp https://m.youtube.com/watch?v=c19cV2b789g --remux-video mp4
+yt-dlp https://m.youtube.com/watch?v=THscO2r8f1Q --remux-video mp4
+yt-dlp https://m.youtube.com/watch?v=FrbUBuZLLH4 --remux-video mp4
+yt-dlp  https://m.youtube.com/watch?v=sSpu6uyldYo --remux-video mp4
+yt-dlp https://m.youtube.com/watch?v=9v--Fg78MqU --remux-video mp4
+
---
+++ list.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+./downgrade "chvrches - good girls (unicorn mix) [JdsmPvZyYCo].mp4" "chvrches - good girls (unicorn mix) [JdsmPvZyYCo]_downgraded.mp4"
+./downgrade "grape skittles [MQjc35p78wE].mp4" "grape skittles [MQjc35p78wE]_downgraded.mp4"
+./downgrade "mazzy star - fade into you (unicorn mix) [FrbUBuZLLH4].mp4" "mazzy star - fade into you (unicorn mix) [FrbUBuZLLH4]_downgraded.mp4"
+./downgrade "funnel pie [HuipBLsURtU].mp4" "funnel pie [HuipBLsURtU]_downgraded.mp4"
